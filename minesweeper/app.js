1c8ebc1328bab45d7c48458af55353646ffcd727
Eugen Sadovnikov <sadeugen@gmail.com>
2023-05-23 00:23:03 +0300

    feat:  Add flag

--- minesweeper/app.js
+++ minesweeper/app.js
@@ -29,14 +29,14 @@ FLAGS.innerHTML = 'Uncleared mines left: <span id=\'flagsCount\'></span>';
   let Arr = [];
   let gameOver = false;
   
-  //create Field of cells
+  //create Field-Table of cells
   function createTable() {
     flagsCount.innerHTML = minesCount;
 
     const minesArr = Array(minesCount).fill('mine');
     const emptyArr = Array(tableSize*tableSize - minesCount).fill('empty');
     const randArr = emptyArr.concat(minesArr).sort(() => 0.5-Math.random());
-    //console.log(randArr.join("|"));
+    console.log(randArr.join("|"));
     //create Array of randon div elem with class empty or mine and id = number elem
     //adding elemets in HTML structure
     for(let i = 0; i < tableSize*tableSize; i++) {
@@ -47,28 +47,74 @@ FLAGS.innerHTML = 'Uncleared mines left: <span id=\'flagsCount\'></span>';
       TAB.appendChild(item);
       /////item-events - onclick & oncontextmenu/////////////////
       item.onclick = function() {
-        item.style.backgroundColor = "red";
+        check(item);
       }
 
       item.oncontextmenu = function() {
-        //alert("oncontextmenu");
-        
+        flag(item);
       }
       /////////////////////////////////////////////////////////
     }
     
-    //found count of mines for every iteam in array and set this nuber as attr - dangerLevel 
+    //found count of mines for every item in array and set this number as attr - dangerLevel 
     for (let i = 0; i < Arr.length; i++) {
-      let dangerLevel = 0;
-
       if (Arr[i].classList.contains('empty')) {
-
-        Arr[i].setAttribute('dangerLevel', dangerLevel);
+        Arr[i].setAttribute('dangerLevel', dangerLevel(i));
       }
     }
   }
   
+  function dangerLevel(itemNumber){
+    rez=0;
+    i=Math.trunc(itemNumber/tableSize);
+    j=(itemNumber % tableSize);
+    iTop=itemNumber-tableSize;iTopLeft=iTop-1;iTopRight=iTop+1;
+    iBottom=itemNumber+tableSize;iBottomLeft=iBottom-1;iBottomRight=iBottom+1;
+    iLeft=itemNumber-1;iRight=itemNumber+1;
+    Top=(iTop >= 0);Bottom=((iBottom) <= (tableSize**2-1));
+    Left=j>0;Right=j<tableSize-1;
+    //top
+    if (Top && Arr[iTop].classList.contains('mine')) rez++;
+    //bottom
+    if (Bottom && Arr[iBottom].classList.contains('mine')) rez++;
+    //left
+    if (Left && Arr[iLeft].classList.contains('mine')) rez++;
+    //left-top
+    if (Left && Top && Arr[iTopLeft].classList.contains('mine')) rez++;
+    //left-bottom
+    if (Left && Bottom && Arr[iBottomLeft].classList.contains('mine')) rez++;
+    //right
+    if (Right && Arr[iRight].classList.contains('mine')) rez++;
+    //right-top
+    if (Right && Top && Arr[iTopRight].classList.contains('mine')) rez++;
+    //right-bottom
+    if (Right && Bottom && Arr[iBottomRight].classList.contains('mine')) rez++;
+    return rez;
+  }
   createTable();
+  //add label - Flag
+  function flag(item) {
+    if (gameOver) return;
+    if (!item.classList.contains('checked') && (flags < minesCount)) {
+      if (!item.classList.contains('flag')) {
+        item.classList.add('flag');
+        item.innerHTML = 'V';
+        flags ++;
+        flagsCount.innerHTML = minesCount - flags;
+        //checkVictory();
+      } else {
+        item.classList.remove('flag');
+        item.innerHTML = '';
+        flags --;
+        flagsCount.innerHTML = minesCount - flags;
+      }
+    }
+  }
+  //check item
+  function check(item){
+    
+  }
+  
 
 
 
